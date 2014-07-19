1cc6f616c61f18f5ae26368762b686e0ade54e9b
rlogiacco <rlogiacco@gmail.com>
2014-07-19 14:25:06 +0200

    improved joystick implementation

--- Joystick.cpp
+++ Joystick.cpp
@@ -6,29 +6,53 @@
  */
 
 #include "Joystick.h"
+#include <SerialDebug.h>
 
-Joystick::Joystick() { }
+Joystick::Joystick() {
+	lastDebounce = 0;
+}
 
-void Joystick::begin(uint8_t x, uint8_t y, uint8_t button) {
-	this->_x = x;
-	this->_y = y;
-	this->_button = button;
-	this->calibrate();
+void Joystick::begin(uint8_t x, uint8_t y, uint8_t button, uint8_t debounce) {
+	this->xPin = x;
+	this->yPin = y;
+	this->buttonPin = button;
+	this->debounce = debounce;
 	pinMode(x, INPUT);
 	pinMode(y, INPUT);
-	pinMode(button, INPUT);
+	pinMode(button, INPUT_PULLUP);
+	buttonRef, buttonState = HIGH;
+	this->calibrate();
 }
 
 void Joystick::calibrate() {
-	_xRef = analogRead(_x);
-	_yRef = analogRead(_y);
-	_buttonRef = digitalRead(_button);
+	xRef = analogRead(xPin);
+	yRef = analogRead(yPin);
+
+	DEBUG("calibrate", xRef, yRef, buttonRef);
 }
 
 int16_t Joystick::x() {
-	return analogRead(_x) - _xRef;
+	return analogRead(xPin) - xRef;
 }
 
 int16_t Joystick::y() {
-	return analogRead(_y) - _yRef;
+	return analogRead(yPin) - yRef;
+}
+
+bool Joystick::button() {
+	uint8_t reading = digitalRead(buttonPin);
+	DEBUG(reading, buttonState);
+	if (reading != buttonState) {
+		// reset the debouncing timer
+		lastDebounce = millis();
+	}
+
+	if ((millis() - lastDebounce) > debounce) {
+		// whatever the reading is at, it's been there for longer
+		// than the debounce delay, so take it as the actual current state:
+		if (reading != buttonState) {
+			buttonState = reading;
+		}
+	}
+	return buttonState != buttonRef;
 }
--- Joystick.h
+++ Joystick.h
@@ -12,14 +12,24 @@
 
 class Joystick {
 public:
+	enum State {
+		NONE,
+		CLICK,
+		LONG_CLICK,
+		DOUBLE_CLICK
+	};
 	Joystick();
-	void begin(uint8_t x, uint8_t y, uint8_t button = 255);
+	void begin(uint8_t x, uint8_t y, uint8_t button = 255, uint8_t debounce = 50);
 	void calibrate();
 	int16_t x();
 	int16_t y();
+	bool button();
 private:
-	uint8_t _x, _y, _button;
-	uint16_t _xRef, _yRef, _buttonRef;
+	uint8_t xPin, yPin, buttonPin;
+	uint16_t xRef, yRef;
+	bool buttonState, buttonRef;
+	uint16_t lastDebounce, lastClick;
+	uint8_t debounce, duration, interval;
 };
 
 #endif /* JOYSTICK_H_ */
